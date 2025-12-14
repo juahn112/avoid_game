a40a2172b6c699911b4aa3e1dd7874010cd0092a
juahn Shin <juahn112@gmail.com>
2025-12-14 21:45:54 +0900

    screen setting and key event active

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
 import pygame
 
 pygame.init()
-width, height = 1280, 720
-screen = pygame.display.set_mode((width, height))
+# width, height = 1280, 720 
+size = [1280,720]
+screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 running = True
+FPS = 60
+background_color = ["red", "yellow", "darkgray","white"]  
+default_color = "black"
+dt = 0
+player_img = ""
 
+player_pos = pygame.Vector2(size[0] // 2, size[1] // 2)
+
+while running:
+    screen.fill(default_color)
+    dt = clock.tick(FPS) / 1000
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False     
+    
+    pygame.draw.circle(screen, "blue", player_pos, 15)
+    
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 200 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 200 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 200 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 200 * dt
+        
+    pygame.display.flip()
+pygame.quit()
