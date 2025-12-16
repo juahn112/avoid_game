e09f3458323411341cd1cf485978b45a66fe0206
신주안 <juahn112@gmail.com>
2025-12-17 06:07:35 +0900

    fix : 플레이어 이동 로직 수정(x축만 허용)

--- main.py
+++ main.py
@@ -4,25 +4,26 @@ pygame.init()
 # width, height = 1280, 720 
 size = [1280,720]
 screen = pygame.display.set_mode(size)
+pygame.display.set_caption("신소명 피하기")
 clock = pygame.time.Clock()
 running = True
 FPS = 60
-background_color = ["red", "yellow", "darkgray","white"]  
-default_color = "black"
-dt = 0
-sound = pygame.mixer.music.load("bgm.ogg")
 
-player_img = pygame.image.load("player.png")
+background_img = pygame.image.load("img/upscaling_image.png")
+background_img = pygame.transform.scale(background_img, size)
+dt = 0
+
+
+player_img = pygame.image.load("img/player.png")
 player_img = pygame.transform.scale(player_img, (75, 75))
 
-enemy_img = pygame.image.load("enemy.png")
+enemy_img = pygame.image.load("img/enemy.png")
 enemy_img = pygame.transform.scale(enemy_img, (50, 50))
 
-player_pos = pygame.Vector2(size[0] // 2, size[1] // 2)
+player_pos = pygame.Vector2(size[0] // 2, 550)
 
 while running:
-    screen.fill(default_color)
-    
+    screen.blit(background_img, (0, 0))
     dt = clock.tick(FPS) / 1000
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -35,14 +36,18 @@ while running:
     screen.blit(player_img, player_pos)
     
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_UP]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_DOWN]:
-        player_pos.y += 300 * dt
+    #if keys[pygame.K_UP]:
+    #    player_pos.y -= 300 * dt
+    #if keys[pygame.K_DOWN]:
+    #    player_pos.y += 300 * dt
     if keys[pygame.K_LEFT]:
         player_pos.x -= 300 * dt
+        if player_pos.x < 0:
+            player_pos.x = 0
     if keys[pygame.K_RIGHT]:
         player_pos.x += 300 * dt
+        if player_pos.x > size[0] - 75:
+            player_pos.x = size[0] - 75
         
     pygame.display.flip()
 pygame.quit()
